a98fc74c6a04b5c263e723ab29c2cf4b02bd94bc
Tobost06 <114383977+Tobost06@users.noreply.github.com>
2024-06-24 20:05:34 +0200

    Adding Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,47 @@
+# Definer banen til loggfilen
+$logFile = "C:\filepath here"
+
+# Funksjon for logging av meldinger til filen
+function Log-Message {
+    param (
+        [string]$message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    "$timestamp - $message" | Out-File -Append $logFile
+}
+
+# Start tidsporing
+function Start-Logging {
+    Log-Message "Startet"
+}
+
+# Stopp tidsporing
+function Stop-Logging {
+    Log-Message "Sluttet"
+}
+
+# Hovedmeny
+while ($true) {
+    Clear-Host
+    Write-Host "Minecraft tidstager"
+    Write-Host "1. Start logging"
+    Write-Host "2. Stopp logging"
+    Write-Host "3. Avslutt"
+    $choice = Read-Host "Velg noe (1, 2, 3)"
+
+    switch ($choice) {
+        1 {
+            Start-Logging
+        }
+        2 {
+            Stop-Logging
+        }
+        3 {
+            Write-Host "Avslutter script"
+            Stop-Process -Id $PID
+        }
+        default {
+            Write-Host "Ugyldig valg. Velg 1, 2, eller 3."
+        }
+    }
+}
